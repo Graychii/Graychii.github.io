42575d045884156c63e637e1d627070042fa905e
Graychii <nassimmansouri02@gmail.com>
2024-05-23 16:32:28 +0100

    hehe

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const siteConfig: SiteConfig = {
   subtitle: ' ',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 315,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
@@ -20,9 +20,9 @@ export const siteConfig: SiteConfig = {
   },
   favicon: [    // Leave this array empty to use the default favicon
     {
-      src: 'favicon/icon.png',    // Path of the favicon, relative to the /public directory
+      src: 'assets/images/icon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+      sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     }
   ]
 }
